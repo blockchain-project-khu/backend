fda3935d9028f5ca0bed80aaaca80771896945d2
EunjinWoo <esther0904@naver.com>
2025-05-09 23:10:08 +0900

    [Feat] 계약 생성 요청 dto 생성

---
+++ src/main/java/blockchain/project/khu/apiserver/domain/rent/dto/request/RentRequestDto.java
@@ -0,0 +1,17 @@
+package blockchain.project.khu.apiserver.domain.rent.dto.request;
+
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+public class RentRequestDto {
+    private Long userId;
+    private Long propertyId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal monthlyRent;
+    private BigDecimal deposit;
+    private Integer paymentDay;
+}
