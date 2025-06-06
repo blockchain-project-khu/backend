5ea1376f3c87b526d5d5bd891156c7ba3f2c70ab
EunjinWoo <esther0904@naver.com>
2025-06-06 15:28:30 +0900

    [Fix] RentResponseDto 수정

--- src/main/java/blockchain/project/khu/apiserver/domain/rent/controller/RentController.java
+++ src/main/java/blockchain/project/khu/apiserver/domain/rent/controller/RentController.java
@@ -1,6 +1,7 @@
 package blockchain.project.khu.apiserver.domain.rent.controller;
 
 import blockchain.project.khu.apiserver.domain.rent.dto.request.RentRequestDto;
+import blockchain.project.khu.apiserver.domain.rent.dto.response.RentResponseDto;
 import blockchain.project.khu.apiserver.domain.rent.entity.Rent;
 import blockchain.project.khu.apiserver.domain.rent.service.RentService;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +16,8 @@ public class RentController {
     private final RentService rentService;
 
     @PostMapping
-    public ResponseEntity<Rent> createRent(@RequestBody RentRequestDto dto) {
+    public ResponseEntity<RentResponseDto.Detail> createRent(@RequestBody RentRequestDto dto) {
         Rent rent = rentService.createRent(dto);
-        return ResponseEntity.ok(rent);
+        return ResponseEntity.ok(RentResponseDto.Detail.from(rent));
     }
 }
