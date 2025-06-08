0af250840d441d24ab2495fe191018223c2b2031
EunjinWoo <esther0904@naver.com>
2025-06-08 23:28:11 +0900

    [Fix] RentPaymentResponseDto 반환 필드 명과 날짜 형식 수정

--- src/main/java/blockchain/project/khu/apiserver/domain/rentPayment/dto/response/RentPaymentResponseDto.java
+++ src/main/java/blockchain/project/khu/apiserver/domain/rentPayment/dto/response/RentPaymentResponseDto.java
@@ -2,6 +2,7 @@ package blockchain.project.khu.apiserver.domain.rentPayment.dto.response;
 
 import blockchain.project.khu.apiserver.domain.rentPayment.entity.RentPayment;
 import blockchain.project.khu.apiserver.domain.rentPayment.enumerate.PaymentStatus;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,17 +19,17 @@ public class RentPaymentResponseDto {
 
     private Long paymentId;
     private Long rentId;
-    private Long tenantId;
+    private Long userId;
     private Long propertyId;
     private BigDecimal amount;
-    private LocalDate paidAt;
+    private @JsonFormat(pattern = "yyyy-MM-dd") LocalDate paidAt;
     private PaymentStatus status;
 
     public static RentPaymentResponseDto fromEntity(RentPayment payment) {
         return RentPaymentResponseDto.builder()
                 .paymentId(payment.getId())
                 .rentId(payment.getRent().getId())
-                .tenantId(payment.getRent().getUser().getId())
+                .userId(payment.getRent().getUser().getId())
                 .propertyId(payment.getRent().getProperty().getId())
                 .amount(payment.getAmount())
                 .paidAt(payment.getPaidAt())
