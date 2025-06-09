f7c8ff7419eed35b56c6a227c0054794eb8a35cd
junho <jh102328@khu.ac.kr>
2025-06-09 21:10:07 +0900

    Feat: Funding 조회 함수 추가

--- src/main/java/blockchain/project/khu/apiserver/domain/funding/repository/FundingRepository.java
+++ src/main/java/blockchain/project/khu/apiserver/domain/funding/repository/FundingRepository.java
@@ -1,6 +1,7 @@
 package blockchain.project.khu.apiserver.domain.funding.repository;
 
 import blockchain.project.khu.apiserver.domain.funding.entity.Funding;
+import blockchain.project.khu.apiserver.domain.property.entity.Property;
 import blockchain.project.khu.apiserver.domain.property.entity.PropertyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ import java.util.List;
 public interface FundingRepository extends JpaRepository<Funding, Long> ,FundingRepositoryCustom{
     Boolean existsByUserIdAndPropertyId(Long userId, Long propertyId);
     List<Funding> findByUserIdAndProperty_Status(Long userId, PropertyStatus status);
+
+    List<Funding> findAllByProperty(Property property);
 }
